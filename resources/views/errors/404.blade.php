1aaff3e2cb40f797df56e20331967d67cd3eac44
flamedeluxe <flamedeluxe@yandex.ru>
2024-12-24 20:09:20 +0800

    404 page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,7 +5,7 @@
         <div class="container _404-container"><img src="images/404.svg" loading="lazy" alt="" class="img-404">
             <h1 class="h-404">К сожалению, страница не&nbsp;найдена…</h1>
             <p class="big-paragraph">Возможно страница была удалена, у нее изменился адрес или она временно недоступна</p>
-            <a href="index.html" class="button w-button">&lt;— вернуться на главную</a>
+            <a href="/" class="button w-button">&lt;— вернуться на главную</a>
         </div>
     </div>
 @endsection
