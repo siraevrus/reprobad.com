45324e6c8d4dfb4a0500776a97f8914ac046991f
andreyfrost <flamedeluxe@yandex.ru>
2025-02-28 18:51:33 +0800

    remove logo re

--- resources/views/site/components/events/item.blade.php
+++ resources/views/site/components/events/item.blade.php
@@ -4,6 +4,8 @@
     <div class="short-event-text">{{ $item->description }}</div>
     <div class="short-event-footer">
         <a href="{{ route('site.events.show', $item->alias) }}" class="button short-event-button w-button">Подробнее —&gt;</a>
+        {{--
         @if($item->logo)<img loading="lazy" src="{{ $item->logo }}" alt="" class="short-event-logo">@endif
+        --}}
     </div>
 </div>
