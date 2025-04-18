75376c792150743975e698e1e0d9a96b80065947
andreyfrost <flamedeluxe@yandex.ru>
2025-04-18 17:54:06 +0800

    fix desc and title

--- resources/views/site/advises/show.blade.php
+++ resources/views/site/advises/show.blade.php
@@ -132,7 +132,7 @@
         <div class="container">
             <div class="short-events"><img loading="lazy" src="images/bg-cal.svg" alt="" class="short-events-bg-image">
                 <div class="section-head-with-detali-button short-events-section">
-                    <h2 class="big-section-h"><strong>Ближайшие</strong> мероприятия по теме</h2>
+                    <h2 class="big-section-h">События и мероприятия</h2>
                     <a href="{{ route('site.events.index') }}" class="more-button w-button">все <span class="only-mobile-text">мероприятия </span>—&gt;</a>
                 </div>
                 @foreach($events as $item)
--- resources/views/site/components/events/item.blade.php
+++ resources/views/site/components/events/item.blade.php
@@ -1,7 +1,7 @@
 <div class="short-event">
     <div class="short-event-date">{{ $item->dates }}</div>
     <a href="{{ route('site.events.show', $item->alias) }}" class="short-event-h">{{ $item->title }}</a>
-    <div class="short-event-text">{{ $item->description }}</div>
+    <div class="short-event-text">{!! $item->description !!}</div>
     <div class="short-event-footer">
         <a href="{{ route('site.events.show', $item->alias) }}" class="button short-event-button w-button">Подробнее —&gt;</a>
         {{--
