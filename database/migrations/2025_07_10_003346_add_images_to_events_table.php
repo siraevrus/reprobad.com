6d3d16078d8197a987c0bf54640c6f9a392254f1
andreyfrost <flamedeluxe@yandex.ru>
2025-07-11 15:39:50 +0800

    logo

--- database/migrations/2025_07_10_003346_add_images_to_events_table.php
+++ database/migrations/2025_07_10_003346_add_images_to_events_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('events', function (Blueprint $table) {
-            $table->string('sort')->nullable();
+            $table->integer('sort')->default(0);
         });
     }
 
