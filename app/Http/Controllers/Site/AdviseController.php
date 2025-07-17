6c27e5fafb4d5f5492f98d070816ed01805b808c
andreyfrost <flamedeluxe@yandex.ru>
2025-07-17 19:45:28 +0800

    3 -> 2 кол-во советов на странице внизу

--- app/Http/Controllers/Site/AdviseController.php
+++ app/Http/Controllers/Site/AdviseController.php
@@ -15,12 +15,12 @@ class AdviseController extends Controller
     public function index(Request $request): View
     {
         $resources = Advise::where('active', 1);
-        
+
         // Фильтрация по категории
         if ($request->get('category')) {
             $resources = $resources->where('category', $request->get('category'));
         }
-        
+
         // Поиск
         if ($request->get('query')) {
             $query = strtolower($request->get('query'));
@@ -30,9 +30,9 @@ class AdviseController extends Controller
                   ->orWhere('content', 'like', '%' . $query . '%');
             });
         }
-        
+
         $resources = $resources->orderBy('created_at', 'desc')->paginate(7);
-        
+
         // Получаем категории с подсчетом количества советов
         $allAdvises = Advise::where('active', 1)->get();
         $categories = Advise::where('active', 1)
@@ -46,23 +46,23 @@ class AdviseController extends Controller
                 ];
             })
             ->values();
-        
+
         $resource = (object)[
             'title' => 'Полезные советы',
             'description' => 'Полезные советы по подготовке к беременности'
-        ]; 
+        ];
         $pageType = 'Advise';
-        
+
         return view('site.advises.index', compact('resources', 'categories', 'resource', 'pageType'));
     }
 
     public function show($alias): View
     {
         $resource = Advise::where('alias', $alias)->where('active', 1)->firstOrFail();
-        $events = Event::where('active', 1)->take(3)->get();
-        
+        $events = Event::where('active', 1)->take(2)->get();
+
         $pageType = 'Advise';
-        
+
         return view('site.advises.show', compact('resource', 'events', 'pageType'));
     }
 
