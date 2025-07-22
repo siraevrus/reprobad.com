68fc4ac989817bca516b822ee19b06f53c4700cf
andreyfrost <flamedeluxe@yandex.ru>
2025-07-22 18:23:56 +0800

    изменил высоту на max-height 380px

--- resources/views/site/complex/show.blade.php
+++ resources/views/site/complex/show.blade.php
@@ -185,7 +185,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tiny-slider/2.9.4/min/tiny-slider.js"></script>
     <style>
         .main-slider img {
-            height: 280px;
+            max-height: 380px;
             margin: auto;
             display: block;
         }
