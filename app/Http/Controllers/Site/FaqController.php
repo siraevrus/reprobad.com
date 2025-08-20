197ddf4e309e2640d4f14dcde85381c2c080b866
andreyfrost <flamedeluxe@yandex.ru>
2025-08-20 14:59:39 +0800

    добавлены events

--- app/Http/Controllers/Site/FaqController.php
+++ app/Http/Controllers/Site/FaqController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
 use App\Models\Faq;
+use App\Models\Event;
 use Illuminate\View\View;
 
 class FaqController extends Controller
@@ -11,12 +12,13 @@ class FaqController extends Controller
     public function index(): View
     {
         $resources = Faq::where('active', 1)->get();
+        $events = Event::where('active', 1)->take(2)->get();
         $pageType = 'faq';
         $resource = (object)[
             'title' => 'Вопросы - ответы',
             'description' => 'Вопросы - ответы'
         ];
 
-        return view('site.faq', compact('resource', 'pageType', 'resources'));
+        return view('site.faq', compact('resource', 'pageType', 'resources', 'events'));
     }
 }
