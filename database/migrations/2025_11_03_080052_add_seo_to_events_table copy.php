45844c537a841e2618e9dd9f9b27bcd9bca6182a
andreyfrost <flamedeluxe@yandex.ru>
2025-11-05 17:43:11 +0800

    fix table

--- database/migrations/2025_11_03_080052_add_seo_to_events_table copy.php
+++ database/migrations/2025_11_03_080052_add_seo_to_events_table copy.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('events', function (Blueprint $table) {
+        Schema::table('points', function (Blueprint $table) {
             $table->text('seo_title')->nullable();
             $table->text('seo_description')->nullable();
         });
@@ -22,7 +22,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('events', function (Blueprint $table) {
+        Schema::table('points', function (Blueprint $table) {
             $table->dropColumn('seo_title');
             $table->dropColumn('seo_description');
         });
